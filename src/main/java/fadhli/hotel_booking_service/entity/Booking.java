983620c9c5912334fa226c506985a5704a3fb873
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-09-25 19:10:16 +0700

    Update github workflows

--- src/main/java/fadhli/hotel_booking_service/entity/Booking.java
+++ src/main/java/fadhli/hotel_booking_service/entity/Booking.java
@@ -20,7 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "booking")
+@Table(name = "bookings")
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
